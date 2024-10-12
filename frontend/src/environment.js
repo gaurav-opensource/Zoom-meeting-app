a0053f3252663c9d35ebb1300d27058e7149874e
gaurav-opensource <gauravopensource95@gmail.com>
2024-10-12 22:58:08 +0530

    Initial commit

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,5 +1,5 @@
 // environment.js
-let IS_PROD = false; // Set to false for local development
+let IS_PROD = true; // Set to false for local development
 const server = IS_PROD ? "https://Zoom-meeting-app.onrender.com" : "http://localhost:8000";
 
 export default server;
